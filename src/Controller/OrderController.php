3c41db03fdbf6df758c2908afcc9d42c76d4a156
Cedric AUDRIT <cedric@audrit.be>
2021-04-16 15:54:03 +0200

    order tunnel

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use App\Form\OrderType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    #[Route('/order', name: 'order.index')]
+    public function index(): Response
+    {
+        if (!$this->getUser()->getAddresses()->getValue())
+        {
+            return $this->redirectToRoute('account.address.add');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
